2a87ad61f94b49cf78062d8a14099d989308813e
Ruslan Safaroff <62283143+NimblePython@users.noreply.github.com>
2025-02-14 20:52:09 +0300

    Да, да, наши любимые динамические массивы...
    Объявлять динамические массивы через статические нельзя...

--- c/practices/practice_2/task_1.c
+++ c/practices/practice_2/task_1.c
@@ -15,7 +15,7 @@ int main()
     printf("Hi!\nPlease enter ammount of students: ");
     scanf("%d", &n);
 
-    struct Student students[n];
+    struct Student* students = malloc(sizeof(struct Student) * n);
 
     for (int i = 0; i < n; i++)
     {
@@ -26,5 +26,6 @@ int main()
     {
         printf("Name: %s, Age: %d, Average mark: %.1f\n", students[i].name, students[i].age, students[i].avg_mark);
     }
+    free(students);
 
 }
